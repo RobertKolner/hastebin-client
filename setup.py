a8322e20bbba9ac745eef79f6c8473629596443d
Robert Kolner <robert.kolner@gmail.com>
2016-12-21 23:06:06 +0100

    Updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     # Metadata
     name='hastebin-client',
-    version='0.1',
+    version='0.1.1',
     description='Command line tool for uploading files to hastebin.com',
     keywords=['hastebin', 'haste', 'client'],
 
     # Origin
     url='https://github.com/RobertKolner/hastebin-client',
-    download_url='https://github.com/RobertKolner/hastebin-client/releases/tag/0.1',
+    download_url='https://github.com/RobertKolner/hastebin-client/releases/tag/0.1.1',
     author='Robert Kolner',
     author_email='robert.kolner@gmail.com',
     license='MIT',
