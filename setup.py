d58a68f7c360f9e9503f20ba099d9b7aaacf46a3
Robert Kolner <robert.kolner@gmail.com>
2016-12-28 21:16:19 +0100

    Bumped version to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     # Metadata
     name='hastebin-client',
-    version='0.1.1',
+    version='0.1.2',
     description='Command line tool for uploading files to hastebin.com',
     keywords=['hastebin', 'haste', 'client'],
 
